ef75527d1d21f17d7eaa2421be705a11e92b004f
freebieswarda-coder <freebies.warda@gmail.com>
2026-02-15 18:01:31 +0500

    Add Drive model download code

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,16 +2,38 @@ import streamlit as st
 import joblib
 import numpy as np
 import pandas as pd
+import os
+import gdown
 
-st.set_page_config(page_title="Melbourne House Price Predictor")
+# -----------------------------
+# DOWNLOAD MODEL IF NOT EXISTS
+# -----------------------------
 
-st.title("🏠 Melbourne House Price Prediction")
+MODEL_PATH = "models/model.pkl"
 
-model_info = joblib.load("models/model.pkl")
+if not os.path.exists(MODEL_PATH):
+    os.makedirs("models", exist_ok=True)
+    
+    url = "https://drive.google.com/uc?id=1Yt_cFwhAKGgilBUHM3ybuI9vQEDq1CQH"
+    gdown.download(url, MODEL_PATH, quiet=False)
+
+# -----------------------------
+# LOAD MODEL
+# -----------------------------
+
+model_info = joblib.load(MODEL_PATH)
 
 model = model_info["model"]
 scaler = model_info["scaler"]
 columns = model_info["columns"]
+
+# -----------------------------
+# STREAMLIT UI
+# -----------------------------
+
+st.set_page_config(page_title="Melbourne House Price Predictor")
+
+st.title("🏠 Melbourne House Price Prediction")
 
 st.sidebar.header("Input Features")
 
